374152f3f1751001337741809601977b9ba48890
Chai <chaicapil@unm.edu>
2023-04-03 14:00:57 -0600

    Little bit of gui

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        TouchScreen touchScreen = new TouchScreen();
+        Scene scene = touchScreen.getScene();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 
     public void readSetUp() {
@@ -10,4 +23,5 @@ public class Main {
     public void parseSetUpInfo() {
 
     }
+
 }
--- src/src/TouchScreen.java
+++ src/src/TouchScreen.java
@@ -1,6 +1,27 @@
-public class TouchScreen {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-    public TouchScreen() {};
+public class TouchScreen {
+    private final double WINDOW_WIDTH = 800;
+    private final double WINDOW_HEIGHT = 600;
+    private Button audioBtn;
+    private Button brightnessBtn;
+    private Button languageBtn;
+    private Button textSizeBtn;
+    private Group root;
+    private Scene scene;
+    public TouchScreen() {
+        this.root = new Group();
+        setScene();
+    }
 
     private void select() {
 
@@ -45,4 +66,65 @@ public class TouchScreen {
     private void castBallot() {
 
     }
+
+    /*
+    ----------------------------
+    BELOW IS CODE USED FOR JAVAFX
+    ----------------------------
+     */
+
+    private void setScene() {
+        //fill root with layout
+        setMenuLayout();
+        //set scene
+        scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+    public Scene getScene() {
+        return scene;
+    }
+
+    private void setMenuLayout() {
+        //Action buttons will be at the bottom of the screen in a horizontal layout
+        HBox actionButtons = new HBox();
+        actionButtons.setSpacing(10);
+        actionButtons.setLayoutX(0);
+        actionButtons.setLayoutY(WINDOW_HEIGHT - 100);
+        //Create buttons
+        Button volumeDown = createButtonWithImage("C:\\Users\\Chai\\Documents\\VotingMachine\\src\\Images\\volumeDown.png");
+        Button volumeUp = createButtonWithImage("C:\\Users\\Chai\\Documents\\VotingMachine\\src\\Images\\162-1623769_volume-up-volume-up-icon.png");
+        Button brightnessDown = createButtonWithImage("C:\\Users\\Chai\\Documents\\VotingMachine\\src\\Images\\brightnessDown.png");
+        Button brightnessUp = createButtonWithImage("C:\\Users\\Chai\\Documents\\VotingMachine\\src\\Images\\brightnessUp.png");
+        Button textSize = createButtonWithImage("C:\\Users\\Chai\\Documents\\VotingMachine\\src\\Images\\textSize.png");
+        //Add buttons to layout
+        actionButtons.getChildren().addAll(volumeDown, volumeUp, brightnessDown, brightnessUp, textSize, languageMenu());
+        //Add layout to root
+        root.getChildren().add(actionButtons);
+    }
+
+    private MenuButton languageMenu() {
+        //Languages may need to be changed from this list
+        String[] languages = {"English", "Spanish", "French", "German", "Italian", "Portuguese", "Russian", "Chinese"};
+        MenuButton language = new MenuButton("Language");
+        for (String lang : languages) {
+            MenuItem languageItem = new MenuItem(lang);
+            languageItem.setOnAction(e -> changeLanguage());
+            language.getItems().add(languageItem);
+        }
+        language.setPrefHeight(50);
+        return language;
+    }
+
+    private Button createButtonWithImage(String pathToImage) {
+        Image image = new Image(pathToImage);
+        ImageView imageView = new ImageView(image);
+        imageView.setFitHeight(50);
+        imageView.setFitWidth(50);
+        Button buttonWithImage = new Button();
+        buttonWithImage.setGraphic(imageView);
+        return buttonWithImage;
+    }
+
+
+
+
 }
