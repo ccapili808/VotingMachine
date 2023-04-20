a0ada9c76832d1453f34df171877ffd928e1178c
Fermin Ramos <ramosfer@unm.edu>
2023-04-19 18:42:15 -0600

    Basic Structure of GUI Prompts/Answer Choices created + Removed "External LED" from TouchScreen

--- src/src/TouchScreen.java
+++ src/src/TouchScreen.java
@@ -1,24 +1,26 @@
+import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 public class TouchScreen {
     private final double WINDOW_WIDTH = 800;
     private final double WINDOW_HEIGHT = 600;
-    private int audioLevel;
-    private int brightnessLevel;
-    private String masterLanguage;
-    private int textSizeLevel;
+    private static int brightnessLevel;
+    private static String masterLanguage;
+    private static int textSizeLevel;
 
     private Group root;
     private Scene scene;
     public TouchScreen() {
         // Default levels
-        audioLevel = 5;
         brightnessLevel = 5;
         masterLanguage = "English";
         textSizeLevel = 5;
@@ -36,30 +38,22 @@ public class TouchScreen {
     }
 
     private void increaseBrightness() {
-        if(brightnessLevel < 10){
-            brightnessLevel += 1;
-        }
+        if(brightnessLevel < 10){brightnessLevel += 1;}
         System.out.println("New Brightness Level " + brightnessLevel);
 
     }
     private void decreaseBrightness(){
-        if(brightnessLevel > 0){
-            brightnessLevel -= 1;
-        }
+        if(brightnessLevel > 0){brightnessLevel -= 1;}
         System.out.println("New Brightness Level " + brightnessLevel);
     }
 
     private void increaseTextSize() {
-        if(textSizeLevel < 10){
-            textSizeLevel += 1;
-        }
+        if(textSizeLevel < 10){textSizeLevel += 1;}
         System.out.println("New Text Size " + textSizeLevel);
 
     }
     private void decreaseTextSize(){
-        if(textSizeLevel > 0){
-            textSizeLevel -= 1;
-        }
+        if(textSizeLevel > 0){textSizeLevel -= 1;}
         System.out.println("New Text Size " + textSizeLevel);
     }
 
@@ -71,12 +65,63 @@ public class TouchScreen {
         System.out.println("New Master Language " + masterLanguage);
     }
 
-    private void nextPage() {
+    /**
+     * TODO: Give this function args (promptString, choices[], etc.) from
+     *   some type of "Items obj". Then just insert each arg in it's respective
+     *   HBox below.
+     */
+    private void displayQuestion(){
+        // TODO: Right here would be a perfect location to translate
+        //  everything before putting them into their HBoxes :)
 
+        int numOpts = 3;  // Creates n-many option choices, not including write-in
+        int xPos = 300;  // Aligning
+        int yPos = 200;  // Aligning
+
+        // TODO: Add ID's to each thingy...how should ID's be formatted?
+        // 1. Prompt
+        HBox promptBox = new HBox();
+        promptBox.getChildren().add(new Text("This is a prompt..."));  // Fluff
+        promptBox.setTranslateX(promptBox.getTranslateX() + xPos);
+        promptBox.setTranslateY(promptBox.getTranslateY() + yPos);
+        promptBox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,null,null)));
+        promptBox.setPadding(new Insets(10));
+        yPos += 50;
+
+        // 2. Choices
+        HBox[] choices = new HBox[numOpts];
+        for(int i = 0; i < numOpts; i++){
+            choices[i] = new HBox();
+            choices[i].getChildren().add(new Text("Answer Choice " + i));  // Fluff
+            choices[i].setTranslateX(choices[i].getTranslateX() + xPos);
+            choices[i].setTranslateY(choices[i].getTranslateY() + yPos);
+            choices[i].setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,null,null)));
+            choices[i].setPadding(new Insets(10));
+
+            yPos += 50;  // Slides box down +50 pixels
+        }
+
+        // 3. Write-in
+        HBox writeIn = new HBox();
+        writeIn.getChildren().add(new Text("Write-in Field..."));  // Fluff
+        writeIn.setTranslateX(writeIn.getTranslateX() + xPos);
+        writeIn.setTranslateY(writeIn.getTranslateY() + yPos);
+        writeIn.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID,null,null)));
+        writeIn.setPadding(new Insets(10));
+
+        // 4. Add questionnaire to GUI display.
+        root.getChildren().addAll(promptBox, writeIn);
+        for(Node c : choices){
+            root.getChildren().add(c);
+        }
+    }
+
+    private void nextPage() {
+        displayQuestion();
     }
 
     private void previousPage() {
-
+        displayQuestion();
     }
 
     private void spoilBallot() {
@@ -100,8 +145,9 @@ public class TouchScreen {
     private void setScene() {
         //fill root with layout
         setAccessibilityLayout();
-        setExternalLED();
 
+        // TEST: Give questions
+        displayQuestion();
 
         //set scene
         scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -156,16 +202,6 @@ public class TouchScreen {
         menuBar.setTranslateX(menuBar.getTranslateX() + 600);
         menuBar.setTranslateY(menuBar.getLayoutY() + 40);
         root.getChildren().add(menuBar);
-    }
-
-
-    private void setExternalLED(){
-        Circle externalLED = new Circle(20);
-        externalLED.setFill(Color.YELLOW);
-        externalLED.setTranslateX(externalLED.getTranslateX() + 30);
-        externalLED.setTranslateY(externalLED.getTranslateY() + 20);
-
-        root.getChildren().add(externalLED);
     }
 
     private Button createButtonWithImage(String pathToImage) {
